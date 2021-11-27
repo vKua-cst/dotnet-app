69a79c867a700681e35cb891c265855a738ed76a
Kuaasar <creusat.chris@gmail.com>
2021-11-27 13:53:23 +0100

    Installed NLog & needed packages

--- App.Web/Program.cs
+++ App.Web/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NLog.Web;
 
 namespace App.Web
 {
@@ -21,6 +17,12 @@ namespace App.Web
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(builder =>
+                {
+                    builder.ClearProviders();
+                    builder.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
     }
 }
